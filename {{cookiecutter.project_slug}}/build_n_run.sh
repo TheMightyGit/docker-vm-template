d6b5f45ea39e5efc042c12b3941aba6b207fba98
Johnny Marshall <the.mighty.git@gmail.com>
2016-12-12 20:34:41 +0000

    Added docker client to container

--- {{cookiecutter.project_slug}}/build_n_run.sh
+++ {{cookiecutter.project_slug}}/build_n_run.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-docker run -d --restart=always -P -v $HOME/.ssh/:/home/{{ cookiecutter.username }}/.ssh/ -v /var/run/docker.sock -v $HOME/.gitconfig:/home/{{ cookiecutter.username }}/.gitconfig -v $(pwd)/nonvolatile:/home/{{ cookiecutter.username }}/nonvolatile $(docker build -t {{ cookiecutter.project_slug }} -q .)
+docker run -d --restart=always -P \
+    -v $HOME/.ssh/:/home/{{ cookiecutter.username }}/.ssh/ \
+    -v /var/run/docker.sock \
+    -v $HOME/.gitconfig:/home/{{ cookiecutter.username }}/.gitconfig \
+    -v $(pwd)/nonvolatile:/home/{{ cookiecutter.username }}/nonvolatile \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    $(docker build -t {{ cookiecutter.project_slug }} -q .)
+
