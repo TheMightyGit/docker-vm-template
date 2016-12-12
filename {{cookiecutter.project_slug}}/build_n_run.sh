f42a1bc6da1badc7d8a933d16fac5de1125afeed
Johnny Marshall <johnny@dabapps.com>
2016-12-12 12:43:01 +0000

    Mount a non volatile area

--- {{cookiecutter.project_slug}}/build_n_run.sh
+++ {{cookiecutter.project_slug}}/build_n_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --restart=always -P -v $HOME/.ssh/:/home/{{ cookiecutter.username }}/.ssh/ -v /var/run/docker.sock -v $HOME/.gitconfig:/home/{{ cookiecutter.username }}/.gitconfig -v nonvolatile:/home/{{ cookiecutter.username }}/nonvolatile $(docker build -q .)
+docker run --restart=always -P -v $HOME/.ssh/:/home/{{ cookiecutter.username }}/.ssh/ -v /var/run/docker.sock -v $HOME/.gitconfig:/home/{{ cookiecutter.username }}/.gitconfig -v $(pwd)/nonvolatile:/home/{{ cookiecutter.username }}/nonvolatile $(docker build -t {{ cookiecutter.project_slug }} -q .)
