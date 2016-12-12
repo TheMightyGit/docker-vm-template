3da77f57a360a24b9d8a97198354369b12248764
Johnny Marshall <johnny@dabapps.com>
2016-12-12 12:23:52 +0000

    Added nonvolatile area

--- {{cookiecutter.project_slug}}/build_n_run.sh
+++ {{cookiecutter.project_slug}}/build_n_run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker run -it --restart=always -P -v $HOME/.ssh/:/home/{{ cookiecutter.username }}/.ssh/ -v /var/run/docker.sock -v $HOME/.gitconfig:/home/{{ cookiecutter.username }}/.gitconfig $(docker build -q .)
+docker run -it --restart=always -P -v $HOME/.ssh/:/home/{{ cookiecutter.username }}/.ssh/ -v /var/run/docker.sock -v $HOME/.gitconfig:/home/{{ cookiecutter.username }}/.gitconfig -v nonvolatile:/home/{{ cookiecutter.username }}/nonvolatile $(docker build -q .)
